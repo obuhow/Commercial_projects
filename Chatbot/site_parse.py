04756d7fd1cbb9caf9b1c5a3df65618cb9a92bbf
ibbenber <ibbenber@student.21-school.ru>
2022-09-26 13:42:40 +0700

    Add make_dict_of_quotes func

--- Chatbot/site_parse.py
+++ Chatbot/site_parse.py
@@ -23,10 +23,34 @@ def parse_main_sitemap(url, mark='http'):
 
     return articles
 
+def parse_article(url):
+    html = urlopen(url).read().decode('utf-8')
+    s = str(html)
+    soup = BeautifulSoup(s, 'html.parser')
+
+    text_particles = []
+    for s in soup.find_all('span', attrs={'style': 'font-weight: 400;'}):
+        text_particles.append(s.string)
+
+    while text_particles.count(None) != 0:
+        text_particles.remove(None)
+
+    text = ' '.join(text_particles)
+
+    return text
+
+def make_dict_of_quotes(hrefs):
+    quotes_dict = {}
+    for a in hrefs:
+        article = parse_article(a)
+        sentences = article.split('.')
+        if len(sentences) > 1:
+            quotes_dict[a] = sentences
+    return quotes_dict
 
 url = "https://sweetspeak.ru/sitemap.html"
 hrefs = parse_main_sitemap(url, 'post')
-print(hrefs)
+quotes_dict = make_dict_of_quotes(hrefs)
 
-
-
+for href, quotes in quotes_dict.items():
+    print(href, quotes, sep=':')
