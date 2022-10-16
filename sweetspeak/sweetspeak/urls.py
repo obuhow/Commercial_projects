2c3b1280cd4782366b8e2bf5233714302fce11cf
obuhow <p@obuhov.pro>
2022-10-16 20:22:04 +0700

    add empty template

--- sweetspeak/sweetspeak/urls.py
+++ sweetspeak/sweetspeak/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('rumors/', include('rumors.urls')),
+    path('', include('rumors.urls')),
 ]
